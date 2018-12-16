e433ba8649a92131bd9157a9addf37790053e013
delitter <45l9tt5r@gmail.com>
2018-12-16 17:05:48 +0800

    webmvcconfig

--- src/main/java/com/manageeasy/me/MeApplication.java
+++ src/main/java/com/manageeasy/me/MeApplication.java
@@ -4,11 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @MapperScan("com.manageeasy.me.Daos")
 @SpringBootApplication
+
 public class MeApplication{
 
     public static void main(String[] args) {
